7638e9ef2f88900877085b3e1b04908a7db8a2be
Jitendra Sahu <jitendra.sahu@tivo.com>
2021-02-03 09:13:02 +0000

    print count in first pass

--- catalogExporter.py
+++ catalogExporter.py
@@ -22,14 +22,19 @@ from vtv_utils import make_dir
 MAGIC_BYTES = 0
 OUTFILE_MAP = {}
 AVAILABLE_IDS = set([])
-AVAILABLE_IDS_FILE = 'available_ids.data'
+AVAILABLE_IDS_FIRST = set([])
+#AVAILABLE_IDS_FILE = 'available_ids.data'
+AVAILABLE_IDS_FILE = ''
 UNIQUE_IDS = {}
 DATE_TODAY = datetime.datetime.now().date()
+TIME_STAMP = round(time.time() * 1000)
+AVAILABLE_IDS_FILE = "available_ids-%s.data" % (TIME_STAMP)
 
 class CatalogExporter(VtvTask):
     def __init__(self):
         VtvTask.__init__(self)
         self.catalogId = self.options.cust_id
+        print(self.catalogId)
         self.config = yaml.load(open(self.options.config_file, 'r'), Loader=yaml.FullLoader)[self.catalogId]
         self.consumer = self.init_consumer()
         self.out_dir = self.config['out_dir']
@@ -77,8 +82,10 @@ class CatalogExporter(VtvTask):
             raise ValueError
 
     def generate_catalog(self):
-        output_file = os.path.join(self.out_dir, "catalog-%s-%s.gz" % (self.catalogId, round(time.time() * 1000)))
-        output_file_fp = gzip.open(output_file, "wt")
+        #output_file = os.path.join(self.out_dir, "catalog-%s-%s.gz" % (self.catalogId, round(time.time() * 1000)))
+        output_file = os.path.join(self.out_dir, "catalog-%s-%s.gz" % (self.catalogId, TIME_STAMP))
+        output_file_fp = OUTFILE_MAP[output_file]
+        available_ids_file = os.path.join(self.out_dir, AVAILABLE_IDS_FILE)
         catalogTopicPartition = TopicPartition("catserver-%s-catalog" % self.catalogId, 0, 0)
         lastMsgToRead = self.consumer.get_watermark_offsets(catalogTopicPartition)[1] - 1
         current_offset = 0
@@ -86,8 +93,7 @@ class CatalogExporter(VtvTask):
         record_list = []
         ps = self.consumer.list_topics("catserver-%s-catalog" % self.catalogId)
         print(ps.topics)
-        cnt, rec, second_pass_empty = 0, 0, 0
-        first_pass_empty, offer_content = 0, 0
+        cnt, rec, offer_content = 0, 0, 0
         batch_size = 500
         first_pass = True
         while current_offset < lastMsgToRead:  
@@ -106,21 +112,31 @@ class CatalogExporter(VtvTask):
 
                 msg_id = msg.key().decode("utf-8")
                 if first_pass:
-                    if msg.value() is None:
-                        first_pass_empty = first_pass_empty + 1
-                        self.logger.info("msg_id: {}, offset_val in dict: {}, msg_offset: {} content null in first_pass".format(msg_id, offset_val, msg.offset()))
+                    existing_offset = UNIQUE_IDS.get(msg_id)
+                    if existing_offset is None:
+                        if msg.value() is not None:
+                            UNIQUE_IDS[msg_id] = msg.offset()
+                            #message, content_type = self.decode_avro(msg.value())
+                            #if content_type in ("VodOffer", "LinearBlock"):
+                            #    self.parse_availability_first(content_type, message)
+                            self.logger.info("msg_id: {}, msg_offset: {} added for the first time in dict".format(msg_id, msg.offset()))
+                    elif msg.value() is None:
+                        del UNIQUE_IDS[msg_id]
+                        self.logger.info("Content is null for msg_id: {}, msg_offset: {}, existing offset: {}. Remove from dict".format(msg_id, msg.offset(), existing_offset))
                     else:
                         UNIQUE_IDS[msg_id] = msg.offset()
+                        #message, content_type = self.decode_avro(msg.value())
+                        #if content_type in ("VodOffer", "LinearBlock"):
+                        #    self.parse_availability_first(content_type, message)
                 else:
-                    offset_val = UNIQUE_IDS.get(msg_id)
-                    if offset_val is None:
-                        self.logger.info("msg_id: {}, msg_offset: {} not present".format(msg_id, msg.offset()))
-                    else:
+                    existing_offset = UNIQUE_IDS.get(msg_id)
+                    if existing_offset is None:
+                        self.logger.info("msg_id: {}, msg_offset: {} not present, msg.value(): {}".format(msg_id, msg.offset(), msg.value()))
+                    elif existing_offset == msg.offset():
                         message, content_type = self.decode_avro(msg.value())
                         if content_type in ("VodOffer", "LinearBlock"):
-                            #print(message)
                             offer_content = offer_content + 1
-                            self.parse_availability(content_type, message)
+                            self.parse_availability(available_ids_file, content_type, message)
                         else:
                             rec = rec + 1
                             record_list.append(OrderedDict([("id", msg_id), ("type", content_type), ("offset", msg.offset()), ("content", message)]))
@@ -134,34 +150,38 @@ class CatalogExporter(VtvTask):
             if first_pass and current_offset == lastMsgToRead:
                 first_pass = False
                 current_offset = 0
+                print("No of records: %d first pass" % cnt)
                 cnt = 0
-                #catalogTopicPartition = TopicPartition("catserver-%s-catalog" % self.catalogId, 0, 0)
                 self.consumer.seek(catalogTopicPartition)
         self.consumer.close()
 
         if len(record_list) > 0:
             output_file_fp.write('\n'.join(json.dumps(record) for record in record_list) + "\n")
 
+        '''
         output_file_fp.close()
-        if os.stat(output_file).st_size > 0:
-            print(s3_utils.upload_file_to_s3(output_file, self.s3Location, self.logger))
+        available_ids_file = os.path.join(self.out_dir, AVAILABLE_IDS_FILE)
+        print(os.path.exists(available_ids_file))
+        if os.stat(output_file).st_size > 0 and os.path.exists(available_ids_file):
+            s3_upload_status = s3_utils.upload_file_to_s3(output_file, self.s3Location, self.logger)
+            available_ids_file_upload = s3_utils.upload_file_to_s3(AVAILABLE_IDS_FILE, self.s3Location, self.logger)
+            print("catalogue upload: %d, available_ids_file_upload: %d" % (s3_upload_status, available_ids_file_upload))
+            self.logger.info("catalogue upload: {}, available_ids_file_upload: {}".format(s3_upload_status, available_ids_file_upload))
+        '''
         print("size in Bytes: %d" % sys.getsizeof(UNIQUE_IDS))
         print("unique records:  %d" % len(UNIQUE_IDS))
         print("Last msg offset: %d" % lastMsgToRead)
         print("No of records: %d" % cnt)
         print("No of written records: %d" % rec)
-        print("No of mismatched entries: %d" % mismatched)
-        print("No of empty records in second pass: %d" % second_pass_empty)
-        print("No of empty records in first pass: %d" % first_pass_empty)
-        print("No of special content records: %d" % offer_content)
+        print("No of offer content records: %d" % offer_content)
+        print("No of available IDs: %d" % len(AVAILABLE_IDS))
 
-    def parse_availability(self, content_type, content_info):
-        filename = os.path.join(self.out_dir, AVAILABLE_IDS_FILE)
+    def parse_availability(self, filename, content_type, content_info):
+        #filename = os.path.join(self.out_dir, AVAILABLE_IDS_FILE)
         fp = OUTFILE_MAP.get(filename)
-        if not fp:
-            fp = open(filename, 'w')
-            OUTFILE_MAP[filename] = fp
-        #content_info = record["content"]
+        #if not fp:
+        #    fp = open(filename, 'w')
+        #    OUTFILE_MAP[filename] = fp
         if not content_info:
             self.logger.info("Content Empty for ID: %s" % self.sk)
             return
@@ -205,13 +225,40 @@ class CatalogExporter(VtvTask):
             value = default
         return value
 
+    def create_files(self):
+        try:
+            catalogue_output_file = os.path.join(self.out_dir, "catalog-%s-%s.gz" % (self.catalogId, TIME_STAMP))
+            catalogue_output_file_p = gzip.open(catalogue_output_file, "wt")
+            OUTFILE_MAP[catalogue_output_file] = catalogue_output_file_p 
+        except Exception as e:
+            print(e)
+            return -1
+
+        try:
+            available_ids_file = os.path.join(self.out_dir, AVAILABLE_IDS_FILE)
+            available_ids_file_p = open(available_ids_file, 'w')
+            OUTFILE_MAP[available_ids_file] = available_ids_file_p
+        except Exception as e:
+            print(e)
+            return -1
+        return 0
+
+
     def close(self):
         for outf in OUTFILE_MAP.values():
             outf.close()
 
+    def upload(self):
+        for key in OUTFILE_MAP:
+            s3_upload_status = s3_utils.upload_file_to_s3(key, self.s3Location, self.logger)
+            print("s3_upload_status: %d" % (s3_upload_status))
+            self.logger.info("s3 upload status: {}".format(s3_upload_status))
+
     def run_main(self):
-        self.generate_catalog()
-        self.close()
+        if(self.create_files() == 0):
+            self.generate_catalog()
+            self.close()
+            self.upload()
 
     def set_options(self):
         config_file = os.path.join(self.system_dirs.VTV_ETC_DIR, 'exporter_cfg.yaml')
